e64bd67254233354de8c9889f07523128ab75fe2
atharvatamta <tamta.atharva@gmail.com>
2026-02-09 22:56:19 +0530

    ssr and csr

--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -1,5 +1,6 @@
 import type { Route } from "./+types/index";
-
+import * as React from 'react';
+import { useState, useEffect } from 'react';
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -10,5 +11,24 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <><div>My App</div></>
+  const now = new Date().toISOString();
+
+  //document and window are not in server and are only in client side
+  if (typeof window === "undefined") {
+    console.log("server render at", now);
+  } else {
+    console.log("client hydration at", now);
+    //hydration means attached js , event listeners to the static html which is sent by server
+  }
+  //we can use hooks here and they will only run on client side and not on server side
+    //because hooks are only run after client hydration
+useEffect(() => {
+    console.log(window);//moving this after the useEffect gives us an error because window is not defined in server side and useEffect only runs in client side after hydration
+}, []);
+
+  return (
+    <>
+      <div>My App</div>
+    </>
+  );
 }
