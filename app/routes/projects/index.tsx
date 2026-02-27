229c16866da47720414cc2213ffc98415e976db4
atharvatamta <tamta.atharva@gmail.com>
2026-02-28 00:07:23 +0530

    added projects cards

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,7 +1,30 @@
-const ProjectsPage = () => {
+import type { Route } from './+types/index';
+import type { Project } from '~/types.ts';
+import ProjectCard from '~/components/project-card';
+export async function loader({
+  request,
+}: Route.LoaderArgs): Promise<{ projects: Project[] }> {
+  const res = await fetch('http://localhost:3001/projects');
+  const data = await res.json();
+console.log('Fetched projects:', data);
+  return { projects: data };
+}
+
+const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
+  const { projects } = loaderData as { projects: Project[] };
+
   return (
     <>
-      <h2 className='text-3xl font-bold mb-8 text-white'>🚀 Projects</h2>
+      <h2 className='text-3xl font-bold mb-8 text-white'>
+        🚀 Projects
+      </h2>
+
+     
+      <div className='grid gap-6 sm:grid-cols-2'>
+        {projects.map((project) => (
+          <ProjectCard key={project.id} project={project} />
+        ))}
+      </div>
     </>
   );
 };
